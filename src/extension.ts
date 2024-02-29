3047f52a323a336df66a19356821158999eb7f08
sebanimm <gks32114221@gmail.com>
2024-02-29 10:20:47 +0900

    왜안될까

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,23 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
 const languages = ["typescript", "typescriptreact"] as const;
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "ts-prettified-extended-interface" is now active!',
-  );
+  console.log("activated");
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   const hoverProvider: vscode.HoverProvider = {
     provideHover(document, position, token) {
+      if (vscode.window.activeTextEditor !== undefined) {
+        const Uri = vscode.window.activeTextEditor.document.uri;
+        const cursorPosition = vscode.window.activeTextEditor.selection.active;
+
+        vscode.commands
+          .executeCommand("vscode.executeHoverProvider", Uri, cursorPosition)
+          .then((a: any) => {
+            console.log(a[0].contents[0].value);
+          });
+      }
+
       return {
         contents: ["hover content"],
       };
@@ -36,5 +36,4 @@ export function activate(context: vscode.ExtensionContext) {
   );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
